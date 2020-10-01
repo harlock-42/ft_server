1b4b45375f3dec05c4c8783e721287cac0b0ce51
tallaire42 <allaire.terry.at@gmail.com>
2020-10-01 15:58:23 +0200

    save

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,26 @@ FROM debian:buster
 
 RUN apt-get update \
 && apt-get install -y vim \
-&& apt-get install -y nginx
+&& apt-get install -y nginx \
+&& apt instatll -y php7.3-fpm \
+&& apt instatll -y php7.3-mysql \
+&& apt instatll -y php7.3-common \
+&& apt instatll -y php7.3-gd \
+&& apt instatll -y php7.3-json \
+&& apt instatll -y php7.3-cli \
+&& apt instatll -y php7.3-curl \
+&& apt instatll -y php7.3-xml \
+&& apt instatll -y php7.3-zip \
+&& apt instatll -y php7.3-mbstring \
+&& service php7.3-fpm start \
+&& apt install -y mariadb-server mariadb-client \
+&& mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('user42');" \
+&& service mysql start
 
 WORKDIR /tmp
 
 COPY ./srcs/nginx/index.html ./
 COPY ./srcs/nginx/default ./
-COPY ./srcs/nginx/www.website.com.conf ./
 COPY ./srcs/wordpress/latest.tar.gz ./
 COPY ./srcs/init.sh ./
 
--- srcs/init.sh
+++ srcs/init.sh
@@ -2,42 +2,21 @@ rm /etc/nginx/sites-available/default
 rm /usr/share/nginx/html/index.html
 cp /tmp/default /etc/nginx/sites-available/
 cp /tmp/index.html /usr/share/nginx/html/
-cp /tmp/www.website.com.conf /etc/nginx/sites-available/
-cp /tmp/www.website.com.conf /etc/nginx/sites-enabled/
 
 # installation
 
-apt update
-apt install -y wget
-
-# installation php
-
-apt -y install  php7.3-fpm \
-&& apt -y install php7.3-mysql \
-&& apt -y install php7.3-common \
-&& apt -y install php7.3-gd \
-&& apt -y install php7.3-json \
-&& apt -y install php7.3-cli \
-&& apt -y install php7.3-curl \
-&& apt -y install php7.3-xml \
-&& apt -y install php7.3-zip \
-&& apt -y install php7.3-mbstring \
 
 # start php
 
-service php7.3-fpm start
 
 # installation mariadb
 
-apt install -y mariadb-server mariadb-client
 
 # mysql setup
 
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASWORD('user42');"
 
 # start mariadb
 
-service mysql start
 
 #wordpress
 
