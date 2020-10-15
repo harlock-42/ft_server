dfda62e32d0f474f7160c47523b191e1268c0d1d
tallaire42 <allaire.terry.at@gmail.com>
2020-10-15 18:35:28 +0200

    save

--- Dockerfile
+++ Dockerfile
@@ -29,15 +29,21 @@ RUN apt-get update \
 && apt install -y mariadb-server mariadb-client \
 && apt install -y ufw
 
+RUN chown -R www-data /var/www/* \
+&& chmod -R 755 /var/www/* \
+&& mkdir -p /var/www/website \
+&& touch /var/www/website/index.php \
+&& echo "<?php phpinfo(); ?>" >> /var/www/website/index.php
+
+
 RUN rm /etc/nginx/sites-available/default \
+&& rm /etc/nginx/sites-enabled/default \
 && rm /usr/share/nginx/html/index.html \
 && cp /tmp/default /etc/nginx/sites-available/ \
-&& cp /tmp/index.html /usr/share/nginx/html/
+&& cp /tmp/index.html /var/www/website/ \
+&& ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 
 WORKDIR ./tmp
-
-RUN mkdir /var/log/nginx/website \
-&& chown -R www-data /var/log/nginx/website/
 
 # SSL
 
@@ -49,7 +55,7 @@ RUN mkdir /etc/nginx/ssl \
 -nodes \
 -out /etc/nginx/ssl/localhost.pem \
 -keyout /etc/nginx/ssl/localhost.key \
--subj "/C=FR/ST=Paris/0=42 School/OU=tallaire/CN=mysite.com"
+-subj "/C=FR/ST=Paris/0=42 School/OU=tallaire/CN=website"
 
 WORKDIR ..
 
@@ -58,22 +64,24 @@ WORKDIR ..
 RUN service mysql start \
 && echo "CREATE USER 'harlock'@'localhost' IDENTIFIED BY 'user42' ;" | mysql -u root \
 && echo "CREATE DATABASE data ; " | mysql -u root \
-&& echo "GRANT ALL PRIVILEGES ON data.* TO 'harlock'@'localhost' WITH GRANT OPTION ;" | mysql -u root \
+&& echo "GRANT ALL PRIVILEGES ON *.* TO 'harlock'@'localhost' WITH GRANT OPTION ;" | mysql -u root \
 && echo "UPDATE mysql.user SET Password=PASSWORD ('user42') WHERE User='harlock' ;" | mysql -u root \
 && echo "FLUSH PRIVILEGES ;" | mysql -u root
-
-# PHP
 
 WORKDIR ./tmp
 
 # Wordpress
 
-RUN mv wordpress /usr/share/nginx/html/
+RUN mv wordpress /var/www/website/
+
+# PHP
 
 RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz \
 && tar xvf phpMyAdmin-5.0.2-all-languages.tar.gz \
 && mv ./config.inc.php ./phpMyAdmin-5.0.2-all-languages/ \
-&& mv ./phpMyAdmin-5.0.2-all-languages /usr/share/nginx/html/wordpress/phpmyadmin
+&& mv ./phpMyAdmin-5.0.2-all-languages /var/www/website/phpmyadmin \
+&& mkdir -p /var/www/website/phpmyadmin/tmp \
+&& chmod -R 775 /var/www/website/phpmyadmin/
 
 WORKDIR ..
 
--- srcs/php/config.inc.php
+++ srcs/php/config.inc.php
@@ -9,7 +9,7 @@
  *
  * @package PhpMyAdmin
  */
-declare(strict_types=1);
+//declare(strict_types=1);
 
 /**
  * This is needed for cookie based authentication to encrypt password in
@@ -31,7 +31,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /**
  * phpMyAdmin configuration storage settings.
@@ -40,26 +40,26 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
 /* User used to manipulate with storage */
 // $cfg['Servers'][$i]['controlhost'] = '';
 // $cfg['Servers'][$i]['controlport'] = '';
-$cfg['Servers'][$i]['controluser'] = 'pma';
-$cfg['Servers'][$i]['controlpass'] = 'user42';
+//$cfg['Servers'][$i]['controluser'] = 'pma';
+//$cfg['Servers'][$i]['controlpass'] = 'user42';
 
 /* Storage database and tables */
-$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-$cfg['Servers'][$i]['relation'] = 'pma__relation';
-$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-$cfg['Servers'][$i]['history'] = 'pma__history';
-$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-$cfg['Servers'][$i]['recent'] = 'pma__recent';
-$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-$cfg['Servers'][$i]['users'] = 'pma__users';
-$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+//$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+//$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+//$cfg['Servers'][$i]['relation'] = 'pma__relation';
+//$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+//$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+//$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+//$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+//$cfg['Servers'][$i]['history'] = 'pma__history';
+//$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+//$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+//$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+//$cfg['Servers'][$i]['recent'] = 'pma__recent';
+//$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+//$cfg['Servers'][$i]['users'] = 'pma__users';
+//$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+//$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
 // $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
 // $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
 // $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
@@ -154,4 +154,4 @@ $cfg['SaveDir'] = '';
  * in the doc/ folder or at <https://docs.phpmyadmin.net/>.
  */
 
-$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';
+$cfg['TempDir'] = '/var/www/website/phpmyadmin/tmp/';
